52490d1093006883f3500e896a9d31ccd2d8cc3d
depish1 <michal.depa@rsqtechnologies.com>
2023-04-07 23:45:09 +0200

    fix /server/health endpoint

--- src/controllers/server.controller.ts
+++ src/controllers/server.controller.ts
@@ -2,5 +2,5 @@ import { Request, Response } from "express";
 
 export const handleHealthEndpoint = (_: Request, res: Response) => {
   console.log("Server is healthy");
-  res.status(200).json({ status: "OK" });
+  return res.status(200).json({ status: "OK" });
 };
--- src/routers/server.router.ts
+++ src/routers/server.router.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from "express";
 
-import { handleHealthEndpoint } from "../controllers/server.controller";
+import { handleHealthEndpoint } from "../controllers/server.controller.js";
 
 const router = express.Router();
 
